290ad60d3db605608161341e9b67f966bad1c0d2
kleberparigi <62912539+kleberparigi@users.noreply.github.com>
2025-05-25 20:23:35 -0300

    Add files via upload

---
+++ PYTHON_2024_02/AULA_01/DETLAHES_SCRIPTS_ESTUDOS.py
@@ -0,0 +1,24 @@
+--AULA 01
+--DADOS E SCRIPTS DIVERSOS PARA ESTUDO
+--DADOS DA AULA 01
+
+Para fazer o download do Arquivo da Aula: 
+https://drive.google.com/drive/folder...
+
+Para acessar o controle de estoque fictício da Hashtag:
+https://dlp.hashtagtreinamentos.com/p...
+
+Instalando o VSCode para Python:   https://www.youtube.com/watch?v=Zy3iaMZbPO8
+ • Como Configurar VSCode para Python  
+
+
+Instalando o Jupyter dentro do VSCode:   https://www.youtube.com/watch?v=2a87xGLDFTQ
+• Como Usar o Jupyter Notebook no VSCode  
+
+Programando horário para rodar seu código:   https://www.youtube.com/watch?v=SxEjHAlCqmo
+• Executar Código em Python Automaticam...  
+
+*********************************************************************
+
+OBSERVAÇÕES DETALHES E SCRIPTS DE ESTUDO:
+
