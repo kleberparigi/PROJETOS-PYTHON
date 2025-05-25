014cbd56faa8e3a721f4c319a8e4477a19e43505
kleberparigi <62912539+kleberparigi@users.noreply.github.com>
2025-05-25 20:25:42 -0300

    Add files via upload

---
+++ PYTHON_2024_07/PYTHON_2024_07.sql
@@ -0,0 +1,60 @@
+CURSO HASHTAG:
+
+CHAVES PARA CERTIFICADO:
+01- https://www.youtube.com/live/qbZsaBMxCJI
+
+
+02-https://www.youtube.com/live/3e_pbrjXdjU
+
+
+03-https://www.youtube.com/live/LFXH6fPYAWA
+
+
+04-https://www.youtube.com/live/woyZIlCrxHs
+
+
+*************************************************************
+
+AULA01:
+https://www.youtube.com/live/qbZsaBMxCJI
+
+Para acessar o controle de estoque fictício da Hashtag:
+https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+
+Para fazer o download do Arquivo da Aula: 
+https://drive.google.com/drive/folders/1ERaraa4ZeXWFWo4K8oNJi4kY6aGZYdzw
+
+
+
+Instalando o VSCode para Python:    https://www.youtube.com/live/Zy3iaMZbPO8
+
+Instalando o Jupyter dentro do VSCode:   https://www.youtube.com/live/2a87xGLDFTQ 
+
+Como Instalar Python no Mac:    https://www.youtube.com/live/vcltUZMcKdc
+
+Programando horário para rodar seu código:   https://www.youtube.com/live/SxEjHAlCqmo
+
+
+*************************************************************
+
+Para validar a sua presença na aula (para o certificado):
+https://dlp.hashtagtreinamentos.com/python/certificado/validacao
+
+*************************************************************
+
+
+Cara no máximo:
+Validar quanto de memória tem alocado para o apache deles, colocar no mínimo 4gb;
+Validar quanto de memória tem alocação para a instancia da base deles, pedir para colocar 5gb, mesmo que gere custo pra aurum;
+Colocar na versão 10.0;
+Pedir para o Luiz rodar os scripts de sanitização;
+
+/
+etc/init.d 
+tomcat_nomecliente.sh 
+
+
+
+
+
